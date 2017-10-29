eb64879f2e8b719e3b8f4768a1fd7458f24de75d
Cedric Pimenta <cedricpim@users.noreply.github.com>
2017-10-29 12:52:36 +0100

    Cleanup MoneyHelper

--- lib/money_helper.rb
+++ lib/money_helper.rb
@@ -1,20 +1,8 @@
-# Module containing helpers to calculate balances from transactions and format
-# money instance to be displayed.
+# Module containing helpers to deal with Money instances.
 module MoneyHelper
   class << self
     def display(money)
       money.format(CONFIGS.dig(:format, :fields, :money, :display))
-    end
-
-    def balance(transactions)
-      expense = transactions.select(&:expense?).sum(&:money)
-      income = transactions.reject(&:expense?).sum(&:money)
-
-      [expense, income].reject(&:zero?).map do |value|
-        formatted_value = MoneyHelper.display(value)
-
-        block_given? ? yield(value, formatted_value) : formatted_value
-      end.join(' | ')
     end
   end
 end
