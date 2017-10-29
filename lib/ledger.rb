c89242caa42c6c5350a2fa320490be7820811b9a
Cedric Pimenta <cedricpim@users.noreply.github.com>
2017-10-29 15:11:21 +0100

    Fix new main ledger file

--- lib/ledger.rb
+++ lib/ledger.rb
@@ -1,39 +1,39 @@
-# require 'colorize'
-# require 'csv'
-# require 'erb'
-# require 'money'
-# require 'money/bank/google_currency'
-# require 'optparse'
-# require 'optparse/date'
-# require 'openssl'
-# require 'pry'
-# require 'readline'
-# require 'terminal-table'
+require 'colorize'
+require 'csv'
+require 'erb'
+require 'money'
+require 'money/bank/google_currency'
+require 'optparse'
+require 'optparse/date'
+require 'openssl'
+require 'pry'
+require 'readline'
 
-# require_relative 'ledger/ui'
+require_relative 'ledger/ui'
 
-# # Money configurations
-# I18n.enforce_available_locales = false
-# Money.default_bank = Money::Bank::GoogleCurrency.new
+# Money configurations
+I18n.enforce_available_locales = false
+Money.default_bank = Money::Bank::GoogleCurrency.new
 
-# # Configurations
-# opts = UI.new.tap(&:run).options
+# Configurations
+opts = UI.new.tap(&:run).options
 
-# puts 'Configuration file not found' or exit unless File.exist?(opts[:config])
+puts 'Configuration file not found' or exit unless File.exist?(opts[:config])
 
-# CONFIGS = YAML.safe_load(ERB.new(File.read(opts[:config])).result, [Symbol]).freeze
+CONFIGS = YAML.safe_load(ERB.new(File.read(opts[:config])).result, [Symbol]).freeze
 
-# Dir.glob(File.join(__dir__, 'lib', 'ledger', '*.rb')).each { |file| require file }
+Dir.glob(File.join(__dir__, 'ledger', '*.rb')).each { |file| require file }
 
+# Namespace for the whole project
 module Ledger
 end
 
-# Ledger.new.create! or exit if ARGV[0] == 'create'
+Ledger::Ledger.new.create! or exit if ARGV[0] == 'create'
 
-# case
-# when opts[:add]        then Ledger.new.add!(opts[:transaction])
-# when opts[:open]       then Ledger.new.open!
-# when opts[:list]       then Printer.new.list
-# when opts[:trips]      then Printer.new(opts[:trip]).trips
-# when opts[:report]     then Printer.new(opts[:report]).report
-# end
+case
+when opts[:add]        then Ledger::Ledger.new.add!(opts[:transaction])
+when opts[:open]       then Ledger::Ledger.new.open!
+when opts[:list]       then Printer.new.list
+when opts[:trips]      then Printer.new(opts[:trip]).trips
+when opts[:report]     then Printer.new(opts[:report]).report
+end
