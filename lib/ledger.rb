02cf5e94e359d726a9adc9c1aa607913fbf70b4a
Cedric Pimenta <cedricpim@users.noreply.github.com>
2017-12-10 13:14:07 +0100

    Order requires

--- lib/ledger.rb
+++ lib/ledger.rb
@@ -8,14 +8,26 @@ require 'readline'
 require 'tempfile'
 require 'xdg'
 
-# Configurations
-require_relative 'ledger/config'
-CONFIG = Ledger::Config.new
-
-Dir.glob(File.join(__dir__, 'ledger', '*.rb')).each { |file| require file }
-
 I18n.enforce_available_locales = false
 Money.default_bank = Money::Bank::GoogleCurrency.new
+
+require_relative 'ledger/config'
+require_relative 'ledger/cli'
+require_relative 'ledger/content'
+require_relative 'ledger/encryption'
+require_relative 'ledger/money_helper'
+require_relative 'ledger/report_builder'
+require_relative 'ledger/printer'
+require_relative 'ledger/report'
+require_relative 'ledger/repository'
+require_relative 'ledger/transaction_builder'
+require_relative 'ledger/trip'
+require_relative 'ledger/version'
+
+# Configurations
+CONFIG = Ledger::Config.new
+
+require_relative 'ledger/transaction'
 
 # Namespace for the whole project
 module Ledger; end
