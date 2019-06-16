fa107de55f53712511281ae97ce3cd9878d9daef
Cedric Pimenta <cedricpim@users.noreply.github.com>
2019-06-16 22:22:05 +0100

    Simplify things

--- lib/ledger.rb
+++ lib/ledger.rb
@@ -10,29 +10,24 @@ require 'readline'
 require 'tempfile'
 require 'xdg'
 
-Money.locale_backend = nil
-
-require_relative 'ledger/config'
-
-CONFIG = Ledger::Config.new
-
-# Set Exchange gem
-if CONFIG.default?
-  Money.default_bank = Money::Bank::OpenExchangeRatesBank.new.tap do |oxr|
-    oxr.cache = CONFIG.exchange[:cache_file]
-    oxr.app_id = CONFIG.exchange[:api_key]
-    oxr.ttl_in_seconds = CONFIG.exchange[:ttl]
-    oxr.update_rates
-  end
-end
-
 %w[actions api filters modules printers reports].each do |folder|
   files = Dir[File.join(__dir__, 'ledger', folder, '**', '*.rb')]
 
-  files.sort { |first, second| second.include?('base') ? 1 : first <=> second }.map { |file| require file }
+  files.sort { |fst, snd| snd.include?('base') ? 1 : fst <=> snd }.map { |file| require file }
 end
 
 Dir[File.join(__dir__, 'ledger', '*.rb')].map { |file| require file }
+
+# Set configuration
+CONFIG = Ledger::Config.new
+
+# Set Exchange gem
+Money.default_bank = Money::Bank::OpenExchangeRatesBank.new.tap do |oxr|
+  oxr.cache = CONFIG.exchange[:cache_file]
+  oxr.app_id = CONFIG.exchange[:api_key]
+  oxr.ttl_in_seconds = CONFIG.exchange[:ttl]
+  oxr.update_rates
+end
 
 # Namespace for the whole project
 module Ledger; end
